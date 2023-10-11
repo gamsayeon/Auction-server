2a862f2606700460b051542751589ba34ea174ba
gamsayeon <aud4551@naver.com>
2023-10-11 16:25:23 +0900

    ENH: issue/9번 UserServiceImpl unit 성공 or 실패 test code 작성

--- src/main/java/com/example/auction_server/dto/BidDTO.java
+++ src/main/java/com/example/auction_server/dto/BidDTO.java
@@ -2,15 +2,13 @@ package com.example.auction_server.dto;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class BidDTO {
